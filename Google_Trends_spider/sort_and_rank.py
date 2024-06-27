7547833755e7dae1304e52ac150d5ff73eaeb974
MartinLundin96 <martin.rikard.lundin@gmail.com>
2024-06-27 16:06:24 +0200

    Delete Google_Trends_spider/sort_and_rank.py

--- Google_Trends_spider/sort_and_rank.py
+++
@@ -1,3 +0,0 @@
-def sort_and_rank(keyword_data):
-    sorted_keyword_data = sorted(keyword_data, key=lambda x: x["Last year vs. last 5 years trend change (%)"], reverse=True)
-    return sorted_keyword_data
